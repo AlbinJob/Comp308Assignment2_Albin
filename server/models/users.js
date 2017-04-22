f149240f4854357eb1036c6f9632e68faab8ba97
AlbinJob <albinjob@gmail.com>
2017-04-22 11:45:24 -0400

    first commit

---
+++ server/models/users.js
@@ -0,0 +1,51 @@
+// require these modules for user models
+
+let mongoose = require("mongoose");
+let Schema = mongoose.Schema;
+let passportLocalMongoose = require("passport-local-mongoose");
+
+//Creating a new schema for user
+
+let UserSchema = new Schema({
+  username: {
+    type: String,
+    default: '',
+    trim: true,
+    required: 'username is required'
+  },
+  password: {
+    type: String,
+    default: '',
+    trim: true,
+    required: 'password is required'
+  },
+  email: {
+    type: String,
+    default: '',
+    trim: true,
+    required: 'email is required'
+  },
+  displayName: {
+    type: String,
+    default: '',
+    trim: true,
+    required: 'Display Name is required'
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  updated: {
+    type: Date,
+    default: Date.now
+  }
+},
+{
+  collection: "users"
+});
+
+let options = ({missingPasswordError: "Wrong Password"});
+
+UserSchema.plugin(passportLocalMongoose, options); //attaching passport local mongoose to the schema
+
+exports.User = mongoose.model('User', UserSchema);
